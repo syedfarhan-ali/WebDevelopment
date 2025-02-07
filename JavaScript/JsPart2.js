ae8b5581b32ef3eabae422ad9bd22d6a1f62afdb
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-02-07 23:11:40 +0530

    cleaning up

---
+++ JavaScript/JsPart2.js
@@ -0,0 +1,59 @@
+// let num = 100;
+// if (num%2 == 0) {
+//     alert("good");
+// }
+// else{
+//     alert("bad");
+// }
+
+// let Name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// alert(`${Name} is ${age} years old`);
+
+// let Quater = 2;
+// switch (Quater) {
+//     case (1):
+//         console.log("Months in Quarter 1 : January,February,March");
+//         break;
+//     case (2):
+//         console.log("Months in Quarter 2 : April,May,June");
+//         break;
+//     case (3):
+//         console.log("Months in Quarter 3 : July,August,September");
+//         break;
+//     case (4):
+//         console.log("Months in Quarter 4 : October,November,December");
+//         break;
+//     default:
+//         console.log("invalid quater");
+// }
+
+// let str = "android";
+// if (((str[0] === 'a') || (str[0] === 'A')) && (str.length > 5)) {
+//     console.log("golden string");
+// }
+// else {
+//     console.log("not golden");
+// }
+
+// let num1 = 50;
+// let num2 = 77;
+// let num3 = 99;
+// if (num1 > num2 && num1 > num3) {
+//     console.log(num1);
+// }
+// else if (num2 > num1 && num2 > num3) {
+//     console.log(num2);
+// }
+// else {
+//     console.log(num3);
+// }
+
+// let number1 = "32";
+// let number2 = "47852";
+// if(number1%10 == number2%10){
+//     console.log("They have same last digit");
+// }
+// else{
+//     console.log("they don't have same last digit");
+// }
---
+++ JavaScript/JsPart3.js
@@ -0,0 +1,33 @@
+//STRING IN JAVASCRIPT ARE IMMUTABLE
+
+// let str = "    hello   ";
+// console.log(str);
+// console.log(str.trim());
+
+// let Name = "farhan";
+// console.log(Name.toUpperCase());
+// let lastName = "ALI";
+// console.log(lastName.toLowerCase());
+
+// let msg = "ILoveCoding";
+// console.log(msg.indexOf("Love"));
+
+// let msg = "     hellow   ";
+// console.log(msg.trim().toUpperCase());
+
+// let message = "ILoveCoding";
+// console.log(message.slice(1,5));
+// console.log(message.slice(5));
+// console.log(message.slice(-3));
+
+// let message = "ILoveCoding";
+// console.log(message.replace("Love","like"));
+
+// let str = "orange";
+// console.log(str.repeat(3));
+
+//Q1
+let msg = "help!";
+console.log(msg.trim().toUpperCase());
+let str = "apnacollege";
+console.log(str.trim(4,11).replace("l","t"));

2f6d47bda60d3853e2127bab51c9f99c23381c56
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-02-10 23:04:17 +0530

    added today's progress

---
+++ JavaScript/JavaScript_Part6/JsPart6.js
@@ -0,0 +1,39 @@
+// function rollDice(){
+//     let roll = Math.floor(Math.random()*6)+1;
+//     console.log(roll);
+    
+// }
+// rollDice();
+// rollDice();
+
+//Function arguments
+// function printName(name){
+//     console.log(name);
+// }
+// let Name = prompt("enter name");
+// printName(Name);
+
+// function addNumbers(num1,num2){
+//     console.log(num1+num2);
+    
+// }
+// addNumbers(5,6);
+
+// function printArray(fruits){
+//     console.log(fruits);
+    
+// }
+// let fruits = ["mango","orange"]
+// printArray(fruits);
+
+// function printName(name,age){
+//     console.log(`${name}'s age is ${age}`);
+// }
+// printName("Rancho",24);
+// printName("Raju");
+// printName(21);
+
+function average(num1,num2,num3) {
+    console.log((num1+num2+num3)/3);
+}
+average(5,9,10);

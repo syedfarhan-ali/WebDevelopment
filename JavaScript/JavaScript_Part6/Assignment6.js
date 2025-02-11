c3611190d43fee28dab4f55b8cec7d885b609c96
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-02-11 23:59:06 +0530

    added today's progress

---
+++ JavaScript/JavaScript_Part6/Assignment6.js
@@ -0,0 +1,62 @@
+// let arr = [4,5,6,7,8,9,10];
+// let n = 7;
+// function largerElements(array,target)
+// {
+//     for(let i = 0;i<array.length;i++)
+//     {
+//         if(array[i] > target){
+//             console.log(array[i]);
+//         }
+//     }
+// }
+// console.log(largerElements(arr,n));
+
+// let string = "abcdabcdefgggh"; // let ans = "abcdefgh";
+// function unique(str) {
+//   let ans = "";
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if(ans.indexOf(char) == -1)
+//     {
+//         ans += char;
+//     }
+//   }
+//   return ans;
+// }
+// console.log(unique(string));
+
+// let country = ["Australia", "Germany", "United States of America"];
+// function longestCountryName(name) {
+//     let largest = "null";
+//     for(let i=0;i<country.length;i++)
+//     {
+//         if(country[i].length > largest.length)
+//         {
+//             largest = country[i];
+//         }
+//     }
+//     return largest;
+// }
+// console.log( longestCountryName(country));
+
+// let str = "afraid";
+// function countVowels(string){
+//     let count = 0;
+//     for(let i=0;i<str.length;i++)
+//     {
+//         if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log( countVowels(str));
+
+let start = 5;
+let end = 10;
+function randomNum(start,end) {
+    return Math.floor(Math.random()*(end-start))+start;
+}
+console.log( randomNum(start,end));
+
--- JavaScript/JavaScript_Part6/JsPart6.js
+++ JavaScript/JavaScript_Part6/JsPart6.js
@@ -1,7 +1,7 @@
 // function rollDice(){
 //     let roll = Math.floor(Math.random()*6)+1;
 //     console.log(roll);
-    
+
 // }
 // rollDice();
 // rollDice();
@@ -15,13 +15,13 @@
 
 // function addNumbers(num1,num2){
 //     console.log(num1+num2);
-    
+
 // }
 // addNumbers(5,6);
 
 // function printArray(fruits){
 //     console.log(fruits);
-    
+
 // }
 // let fruits = ["mango","orange"]
 // printArray(fruits);
@@ -33,7 +33,106 @@
 // printName("Raju");
 // printName(21);
 
-function average(num1,num2,num3) {
-    console.log((num1+num2+num3)/3);
-}
-average(5,9,10);
+// function average(num1,num2,num3) {
+//     console.log((num1+num2+num3)/3);
+// }
+// average(5,9,10);
+// let number = 5;
+// printMultiplicationTable(number);
+// function printMultiplicationTable(num){
+//     for(let i = 1;i <= 10;i++)
+//     {
+//         console.log(i*num);
+
+//     }
+// }
+// function printOneToN(num){
+//     let sum = 0;
+//     for(let i=1;i<=num;i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+// let number = 100;
+// console.log(printOneToN(number));
+
+// function concatinate(array) {
+//     let str = "";
+//   for (let i = 0; i < array.length; i++) {
+//     str = str+arr[i];
+//   }
+//   return str;
+// }
+// let arr = ["hello","world","I","am","Kira"];
+// console.log( concatinate(arr));
+
+// let sum = function(a,b){
+//     return a+b;
+// }
+// console.log(sum(1,3));
+
+// function multipleGreet(func, n) {
+//   for (let i = 1; i <= n; i++) {
+//     func();
+//   }
+// }
+// let greet = function () {
+//   console.log("America ya!");
+// };
+// multipleGreet(greet,10); 
+
+// function odd(){
+//     console.log("odd");
+        
+// }
+// function even(){
+        
+//     console.log("even");
+// }
+// function evenOdd(str)
+// {
+//     if (str == "odd") {
+//         return odd();
+//     }
+//     else if(str == "even")
+//     {
+//         return even();
+//     }
+//     else
+//     {
+//         console.log("wrong input");
+        
+//     }
+// }
+// let string = prompt("Enter value");
+
+// evenOdd(string);
+
+// const calculator = {
+//     add: function(a,b){
+//         return a+b;
+//     },
+//     sub: function(a,b){
+//         return a-b;
+//     },
+//     mul: function(a,b){
+//         return a*b;
+//     }
+
+// };
+// console.log(calculator.sub(5,3));
+
+// const calculator = {
+//     add(a,b){
+//         return a+b;
+//     },
+//     sub(a,b){
+//         return a-b;
+//     },
+//     mul(a,b){
+//         return a*b;
+//     }
+
+// };
+// console.log(calculator.add(5,3));
+

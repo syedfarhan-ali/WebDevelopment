aaeda07591a2942fd1f313d056113e743e0fba6b
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-03-09 09:36:10 +0530

    added backend 1

---
+++ Backend_1(Node.js)/math.js
@@ -0,0 +1,28 @@
+// const sum = (a,b) => a+b;
+// const mul = (a,b) => a*b;
+// const g = 9.8;
+// const PI = 3.14;
+// module.exports.sum = (a,b) => a+b;
+// module.exports.mul = (a,b) => a*b;
+// module.exports.g = 9.8;
+// module.exports.PI = 3.14;
+
+// let obj = {
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     PI:PI
+// };
+
+// module.exports = {
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     PI:PI
+//     };
+// module.exports = obj;
+
+export const sum = (a,b) => a+b;
+export const mul = (a,b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
---
+++ Backend_1(Node.js)/script.js
@@ -0,0 +1,30 @@
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//     console.log("hello ",i);
+// }
+// console.log("bye");
+
+// console.log(process.argv);
+// let args = process.argv;
+
+// for (let i = 2; i < args.length; i++) {
+//     console.log("hello & welcome to ",args[i]);
+// }
+
+// Module 
+// const math = require("./math");
+// console.log(math.sum(2,5));
+// console.log(math.mul(2,5));
+// console.log(math.PI);
+// console.log(math.g);
+
+//Fruits
+
+// const info = require("./Fruits");
+// console.log(info);
+// console.log(info[0]);
+// console.log(info[0].name);
+
+import {sum,PI} from "./math.js";
+console.log(sum(1,2));
+console.log(PI);

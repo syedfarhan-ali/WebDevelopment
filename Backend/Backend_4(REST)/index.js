34c9a445090ed83d227021c8b610342dfbeb18e2
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-03-12 09:31:10 +0530

    added REST

---
+++ Backend/Backend_4(REST)/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(methodOverride('_method'));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "Rancho",
+    content: "Success ke peeche mt bhago, Excellence ke peeche bhago!",
+  },
+  {
+    id: uuidv4(),
+    username: "Farhan",
+    content: "Abba nhi manege",
+  },
+  {
+    id: uuidv4(),
+    username: "Raju",
+    content: "Behen ki shadi bhi to karwani h",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  console.log(req.body);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id",(req,res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("http://localhost:8080/posts");
+});
+
+app.get("/posts/:id/edit",(req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs",{post}); 
+});
+
+app.delete("/posts/:id",(req,res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});

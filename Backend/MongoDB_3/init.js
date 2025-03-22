80707a3cfe36ad625cdd00ddda8f0d7b5d18c63e
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-03-22 10:55:00 +0530

    added new chat project

---
+++ Backend/MongoDB_3/init.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+    {
+        from: "rancho",
+        to: "raju",
+        msg: "bhindi 19₹ kilo",
+        created_at: new Date(),
+    },
+    {
+        from: "farhan",
+        to: "rancho",
+        msg: "abba nhi manege",
+        created_at: new Date(),
+    },
+    {
+        from: "farhan",
+        to: "chatur",
+        msg: "ello",
+        created_at: new Date(),
+    },
+    {
+        from: "raju",
+        to: "rancho",
+        msg: "behen ki shadi karani h",
+        created_at: new Date(),
+    },
+    
+];
+
+Chat.insertMany(allChats);

874551379ab4859e74f6284078fa4b5610f5d18a
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-03-24 09:28:09 +0530

    added stating of major project

---
+++ MajorProject/models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: { type: String },
+        url: {
+            type: String,
+            default:
+                "https://unsplash.com/photos/airplane-on-ground-surrounded-with-trees-G85VuTpw6jg",
+            set: (v) =>
+                v === ""
+                    ? "https://unsplash.com/photos/airplane-on-ground-surrounded-with-trees-G85VuTpw6jg"
+                    : v,
+        },
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
